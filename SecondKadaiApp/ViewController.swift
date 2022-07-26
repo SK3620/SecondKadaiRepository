be634e445e5e7db711155a6df11854e5ecf70def
kenta.suzuki <kenta.suzuki@techacademy.jp>
2022-07-26 15:09:26 +0900

    Label, TextField, button を追加した。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textField.placeholder = "名前"
+               
+        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        let viewController2: ViewController2 = segue.destination as! ViewController2
+        
+        viewController2.textString = textField.text!
+        
     }
 
-
+    @IBAction func unwind(segue: UIStoryboardSegue){
+    }
+    
 }
 
---
+++ SecondKadaiApp/ViewController2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController2.swift
+//  SecondKadaiApp
+//
+//  Created by 鈴木健太 on 2022/07/26.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var textString: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = "こんにちは\n\(textString)さん"
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
